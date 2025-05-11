fdb35b9d3c0184b21a2d77cea174ec6205073631
Sandro Luan <67851048+Sandroluan@users.noreply.github.com>
2025-05-11 20:52:16 -0300

    .

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 #* FLASK = FRAMEWORK WEB
 #* RENDER TEMPLATE = CARREGA OS O HTML
 #* REQUEST = ACESSA DADOS ENVIADO PELO FORM
@@ -16,7 +15,7 @@ db = SQLAlchemy(app) #!CRIA O OBJETO DO BANCO DE DADOS PARA MANIPULAR O BANCO
 
 #!MODELO (TABELA DO BANCO)
 #!CRIA A CLASSE DO BANCO DE DADOS || DEFINE O ID/NOME/EMAIL EM 3 COLUNAS NA TABELA || db.model indica que a classe representea uma tabela
-class usuario(db.Model):
+class Usuario(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   nome = db.Column(db.String(100))
   email = db.Column(db.String(100))
@@ -27,15 +26,15 @@ class usuario(db.Model):
 
 @app.route('/') 
 def index():
-  usuario = usuario.query.all()
-  return render_template('index.html', usuario=usuario) #!ENVIA OS DADOS PARA O HTML
+  usuarios = Usuario.query.all()
+  return render_template('index.html', usuarios=usuarios) #!ENVIA OS DADOS PARA O HTML
 
 @app.route('/novo', methods=['GET', 'POST'])  #!SE FOR "POST" PEGA OS DADOS DO FORMULARIO CRIA E SALVA NO BANCO DE DADOS || SE FOR "GET" APENAS MOSTRA O FORMULARIO VAZIO
 def novo (): #!CRIANDO UM USUARIO NOVO
   if request.method == 'POST':
     nome = request.form['nome']
     email = request.form['email']
-    novo_usuario = usuario(nome = nome, email=email)
+    novo_usuario = Usuario(nome = nome, email=email)
     db.session.add(novo_usuario)
     db.session.commit()
     return redirect(url_for('index'))
@@ -43,7 +42,7 @@ def novo (): #!CRIANDO UM USUARIO NOVO
 
 @app.route('/editar/<int:id>', methods=['GET', 'POST']) 
 def editar(id):#!BUSCA O USUARIO PELO ID || SE FOR 'POST' ATUALIZA OS DADOS NO BANCO || SE FOR 'GET' MOSTRA OS DADOS PREENCHIDOS COM OS DADOS ATUAIS
-  usuario=usuario.query.get_or_404(id)
+  usuario=Usuario.query.get_or_404(id)
   if request.method == 'POST':
     usuario.nome = request.form['nome']
     usuario.email = request.form['email']
@@ -53,12 +52,12 @@ def editar(id):#!BUSCA O USUARIO PELO ID || SE FOR 'POST' ATUALIZA OS DADOS NO B
 
 @app.route('/deletar/<int:id>')#!BUSCA O USUARIO PELO ID 
 def deletar (id): #!BUSCA PELO ID E REMOVE O BANCO DO DADO
-  usuario=usuario.query.get_or_404(id)
+  usuario=Usuario.query.get_or_404(id)
   db.session.delete(usuario)
   db.session.commit()
   return redirect(url_for('index'))
   
-  if __name__ == '__main__':
-    with app.app_context():
+if __name__ == '__main__':
+  with app.app_context():
       db.create_all()
-    app.run(debug=True)
+  app.run(debug=True)
